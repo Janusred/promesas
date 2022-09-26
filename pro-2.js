1aa7c983c4d5ed0d6ba5fb2d1475de01dc4c8e6b
Janusred <horaciok300@hotmail.com>
2022-09-26 02:33:00 -0500

    Manejo de errores

--- pro-2.js
+++ pro-2.js
@@ -1,22 +1,26 @@
 
 
-function sumaUno( numero , callback) {
+function sumaUno( numero ) {
  var promesa = new Promise( function(resolve, reject){
+    console.log(numero);
+if (numero >= 7){
+    reject('El numero es muy alto');
+}
+
     setTimeout(function(){
         resolve( numero + 1);
-    },800);
+    },400);
  }) ;
     return promesa;
 }
   
-sumaUno( 5 ).then(nuevoNumero =>{
-    console.log(nuevoNumero);
-    return sumaUno(nuevoNumero);
+sumaUno( 5 )
+   .then( sumaUno)
+    .then( sumaUno)
+    .then( nuevoNumero => {
+        console.log(nuevoNumero);
 })
-.then( nuevoNumero => {
-    console.log(nuevoNumero);
-    return sumaUno(nuevoNumero);
-})
-.then( nuevoNumero => {
-    console.log(nuevoNumero);
+.catch( error => {
+    console.log('Error en promesa');
+    console.log('error');
 });
